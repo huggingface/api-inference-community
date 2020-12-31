e227f36b5a260bd05ace7dfb25e9641ed27739da
Julien Chaumond <julien@huggingface.co>
2020-12-31 13:47:38 +0100

    Support for multiple models

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
 import time
+from typing import Any, Dict
 
 import soundfile
 import uvicorn
 from espnet2.bin.tts_inference import Text2Speech
 from starlette.applications import Starlette
 from starlette.requests import Request
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, JSONResponse
 from starlette.routing import Route
+
+
+HF_HEADER_COMPUTE_TIME = "x-compute-time"
+
 
 EXAMPLE_TTS_EN_MODEL_ID = (
     "julien-c/ljspeech_tts_train_tacotron2_raw_phn_tacotron_g2p_en_no_space_train"
 )
+EXAMPLE_TTS_ZH_MODEL_ID = "julien-c/kan-bayashi_csmsc_tacotron2"
 
 
-model = Text2Speech.from_pretrained(
-    EXAMPLE_TTS_EN_MODEL_ID,
-    device="cpu"
-)
+MODELS: Dict[str, Any] = {}
+start_time = time.time()
+for model_id in (EXAMPLE_TTS_EN_MODEL_ID, EXAMPLE_TTS_ZH_MODEL_ID):
+    model = Text2Speech.from_pretrained(
+        model_id,
+        device="cpu"
+    )
+    MODELS[model_id] = model
 
-print("model.loaded")
+print("models.loaded", time.time() - start_time)
+
+
+def home(request: Request):
+    return JSONResponse({"ok": True})
 
 
 async def post_inference(request: Request):
-    body = await request.json()
+    start = time.time()
+    try:
+        body = await request.json()
+    except:
+        return JSONResponse(status_code=400, content="Invalid JSON body")
+    model_id = request.path_params["model_id"]
     print(body)
-    outputs = model(body["text"])
+    text = body["text"]
+    _model = MODELS[model_id]
+    outputs = _model(text)
     speech = outputs[0]
     filename = "out-{}.wav".format(int(time.time() * 1e3))
-    soundfile.write(filename, speech.numpy(), model.fs, "PCM_16")
-    return FileResponse(filename)
+    soundfile.write(filename, speech.numpy(), _model.fs, "PCM_16")
+    return FileResponse(filename, headers={
+        HF_HEADER_COMPUTE_TIME: "{:.3f}".format(time.time() - start)
+    })
 
 
 routes = [
-    Route('/models/{model_id}', post_inference, methods=["POST"]),
+    Route('/', home),
+    Route('/models/{model_id:path}', post_inference, methods=["POST"]),
 ]
 
 app = Starlette(debug=True, routes=routes)
@@ -42,4 +66,5 @@ if __name__ == "__main__":
 
 
 # curl -XPOST --data '{"text": "My name is Julien"}' http://127.0.0.1:8000/models/foo | play -
+# curl -XPOST --data '{"text": "请您说得慢些好吗"}' http://127.0.0.1:8000/models/julien-c/kan-bayashi_csmsc_tacotron2 | play -
 # curl -XPOST --data '{"text": "My name is Julien"}' https://api-audio.huggingface.co/models/foo | play -
