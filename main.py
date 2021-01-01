5495729b5b683d81ced8f0df62bf5d386a234f45
Julien Chaumond <julien@huggingface.co>
2021-01-01 15:25:09 +0100

    Add cors middleware now that it's deployed on EKS

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import soundfile
 import uvicorn
 from espnet2.bin.tts_inference import Text2Speech
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import FileResponse, JSONResponse
 from starlette.routing import Route
@@ -51,11 +53,21 @@ async def post_inference(request: Request):
 
 
 routes = [
-    Route('/', home),
-    Route('/models/{model_id:path}', post_inference, methods=["POST"]),
+    Route("/", home),
+    Route("/models/{model_id:path}", post_inference, methods=["POST"]),
 ]
 
-app = Starlette(debug=True, routes=routes)
+middlewares = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        expose_headers=["*"],
+    )
+]
+
+app = Starlette(debug=True, routes=routes, middleware=middlewares)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
