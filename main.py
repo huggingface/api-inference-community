ae27dfdbb21cc42a71a42068f598ad397d3c3c15
Julien Chaumond <julien@huggingface.co>
2021-02-11 17:48:52 +0100

    PoC: Inference for `timm` models (#7)
    * "Cheat" for now
    
    i.e. load models from timm not using huggingface_hub
    
    cc @sgugger
    
    * local sample image
    
    * Examples, almost like ~unit tests 😂
    
    * Opening a PIL.Image from an HTTP request!
    
    * Looks like they're not in eval mode by default
    
    * Use torchvision transform to handle input data
    
    * imagenet labels
    
    * black
    
    * softmax + topk
    
    * Add two more examples
    
    * uncomment stuff

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import json
 import tempfile
 import time
+from io import BytesIO
 from mimetypes import guess_extension
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import librosa
+import requests
 import soundfile
+import timm
 import torch
 import uvicorn
 from asteroid import separate
 from asteroid.models import BaseModel as AsteroidBaseModel
 from espnet2.bin.asr_inference import Speech2Text
 from espnet2.bin.tts_inference import Text2Speech
+from PIL import Image
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
@@ -42,11 +47,16 @@ WAV2VEV2_MODEL_IDS = [
     # "facebook/wav2vec2-large-960h-lv60-self",
 ]
 
+with open("data/imagenet-simple-labels.json") as f:
+    IMAGENET_LABELS: List[str] = json.load(f)
+## ^ from gh.com/anishathalye/imagenet-simple-labels
+
 
 TTS_MODELS: Dict[str, AnyModel] = {}
 ASR_MODELS: Dict[str, AnyModel] = {}
 SEP_MODELS: Dict[str, AnyModel] = {}
 ASR_HF_MODELS: Dict[str, Tuple[AnyModel, AnyTokenizer]] = {}
+TIMM_MODELS: Dict[str, torch.nn.Module] = {}
 
 start_time = time.time()
 for model_id in (EXAMPLE_TTS_EN_MODEL_ID, EXAMPLE_TTS_ZH_MODEL_ID):
@@ -63,6 +73,12 @@ for model_id in WAV2VEV2_MODEL_IDS:
     tokenizer = Wav2Vec2Tokenizer.from_pretrained(model_id)
     ASR_HF_MODELS[model_id] = (model, tokenizer)
 
+TIMM_MODELS["julien-c/timm-dpn92"] = timm.create_model("dpn92", pretrained=True).eval()
+TIMM_MODELS["sgugger/resnet50d"] = timm.create_model(
+    "resnet50d", pretrained=True
+).eval()
+# ^ Those are not in eval mode by default
+
 print("models.loaded", time.time() - start_time)
 
 
@@ -76,6 +92,7 @@ def list_models(_):
         **ASR_MODELS,
         **SEP_MODELS,
         **{k: v[0] for k, v in ASR_HF_MODELS.items()},
+        **TIMM_MODELS,
     }
     return JSONResponse({k: v.__class__.__name__ for k, v in all_models.items()})
 
@@ -127,7 +144,7 @@ async def post_inference_asr(request: Request, model: AnyModel):
             except Exception as exc:
                 return JSONResponse(
                     {"ok": False, "message": f"Invalid audio: {exc}"}, status_code=400
-                )    
+                )
 
     if len(speech.shape) > 1:
         # ogg can take dual channel input -> take only first input channel in this case
@@ -226,6 +243,65 @@ async def post_inference_sep(request: Request, model: AnyModel):
     )
 
 
+async def post_inference_timm(request: Request, model: torch.nn.Module):
+    start = time.time()
+
+    content_type = request.headers["content-type"]
+
+    if content_type == "application/json":
+        body = await request.json()
+        if "url" not in body:
+            return JSONResponse(
+                {"ok": False, "message": f"Invalid json, no url key"}, status_code=400
+            )
+        url = body["url"]
+        img = Image.open(requests.get(url, stream=True).raw)
+    else:
+        body = await request.body()
+        try:
+            img = Image.open(BytesIO(body))
+        except Exception as exc:
+            print(exc)
+            return JSONResponse(
+                {"ok": False, "message": f"Unable to open image from request"},
+                status_code=400,
+            )
+
+    img = img.convert("RGB")
+
+    # Data handling config
+    config = model.default_cfg
+
+    if isinstance(config["input_size"], tuple):
+        img_size = config["input_size"][-2:]
+    else:
+        img_size = config["input_size"]
+
+    transform = timm.data.transforms_factory.transforms_imagenet_eval(
+        img_size=img_size,
+        interpolation=config["interpolation"],
+        mean=config["mean"],
+        std=config["std"],
+    )
+
+    input_tensor = transform(img)
+    input_tensor = input_tensor.unsqueeze(0)
+    # ^ batch size = 1
+    with torch.no_grad():
+        output = model(input_tensor)
+
+    probs = output.squeeze(0).softmax(dim=0)
+
+    values, indices = torch.topk(probs, k=5)
+
+    labels = [IMAGENET_LABELS[i] for i in indices]
+
+    return JSONResponse(
+        [{"label": label, "score": float(values[i])} for i, label in enumerate(labels)],
+        headers={HF_HEADER_COMPUTE_TIME: "{:.3f}".format(time.time() - start)},
+    )
+
+
 async def post_inference(request: Request) -> JSONResponse:
     model_id = request.path_params["model_id"]
 
@@ -244,6 +320,10 @@ async def post_inference(request: Request) -> JSONResponse:
     if model_id in SEP_MODELS:
         model = SEP_MODELS.get(model_id)
         return await post_inference_sep(request, model)
+
+    if model_id in TIMM_MODELS:
+        model = TIMM_MODELS.get(model_id)
+        return await post_inference_timm(request, model)
 
     return JSONResponse(status_code=404, content="Unknown or unsupported model")
 
@@ -274,15 +354,18 @@ if __name__ == "__main__":
 # wget https://assets.amazon.science/c2/65/08e161cb4e96a7e007d6c3a4fef5/sample02-orig.wav
 
 
+# ================
 # TTS example:
 # curl -XPOST --data '{"text": "My name is Julien"}' http://127.0.0.1:8000/models/julien-c/ljspeech_tts_train_tacotron2_raw_phn_tacotron_g2p_en_no_space_train | play -
 # curl -XPOST --data '{"text": "请您说得慢些好吗"}' http://127.0.0.1:8000/models/julien-c/kan-bayashi_csmsc_tacotron2 | play -
 # or in production:
 # curl -XPOST --data '{"text": "My name is Julien"}' http://api-audio.huggingface.co/models/julien-c/ljspeech_tts_train_tacotron2_raw_phn_tacotron_g2p_en_no_space_train | play -
 
+# ================
 # ASR example:
 # curl -i -H "Content-Type: audio/x-wav" -XPOST --data-binary '@samples/sample02-orig.wav' http://127.0.0.1:8000/models/julien-c/mini_an4_asr_train_raw_bpe_valid
 
+# ================
 # ASR wav2vec example:
 # curl -i -H "Content-Type: audio/wav"  -XPOST --data-binary '@samples/sample02-orig.wav' http://127.0.0.1:8000/models/facebook/wav2vec2-base-960h
 # curl -i -H "Content-Type: audio/flac" -XPOST --data-binary '@samples/sample1.flac'      http://127.0.0.1:8000/models/facebook/wav2vec2-base-960h
@@ -292,5 +375,16 @@ if __name__ == "__main__":
 # or in production:
 # curl -i -H "Content-Type: audio/wav"  -XPOST --data-binary '@samples/sample02-orig.wav' http://api-audio.huggingface.co/models/facebook/wav2vec2-base-960h
 
+# ================
 # SEP example:
 # curl -XPOST --data-binary '@samples/sample02-orig.wav' http://127.0.0.1:8000/models/mhu-coder/ConvTasNet_Libri1Mix_enhsingle | play -
+
+# ================
+# TIMM examples:
+# curl -i -H "Content-Type: image/jpeg"        -XPOST --data-binary '@samples/plane.jpg'       http://127.0.0.1:8000/models/julien-c/timm-dpn92
+# curl -i -H "Content-Type: image/jpeg"        -XPOST --data-binary '@samples/plane.jpg'       http://127.0.0.1:8000/models/sgugger/resnet50d
+# curl -i -H "Content-Type: application/json"  -XPOST --data        '{"url": "https://i.picsum.photos/id/543/536/354.jpg?hmac=O-U6guSk3J8UDMCjnqQHaL8EAOR9yHXZtgA90Bf5UTc"}'      http://127.0.0.1:8000/models/julien-c/timm-dpn92
+# curl -i -H "Content-Type: application/json"  -XPOST --data        '{"url": "https://huggingface.co/front/assets/transformers-demo.png"}'      http://127.0.0.1:8000/models/sgugger/resnet50d
+
+# or in production:
+# curl -i -H "Content-Type: application/json"  -XPOST --data        '{"url": "https://huggingface.co/front/assets/transformers-demo.png"}'      http://api-audio.huggingface.co/models/sgugger/resnet50d
