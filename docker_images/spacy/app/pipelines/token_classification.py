24724540b3f65073b99a27fb5ebab1fca8ddaa04
Nicolas Patry <patry.nicolas@protonmail.com>
2022-05-05 13:08:05 +0200

    Enable the override to take place (API needs to bypass WAF).

--- docker_images/spacy/app/pipelines/token_classification.py
+++ docker_images/spacy/app/pipelines/token_classification.py
@@ -18,7 +18,8 @@ class TokenClassificationPipeline(Pipeline):
                 f"Invalid model_id: {model_id}. It should have a namespace (:namespace:/:model_name:)"
             )
         namespace, model_name = full_model_path
-        package = f"https://huggingface.co/{namespace}/{model_name}/resolve/main/{model_name}-any-py3-none-any.whl"
+        hf_endpoint = os.getenv("HF_ENDPOINT", "https://huggingface.co")
+        package = f"{hf_endpoint}/{namespace}/{model_name}/resolve/main/{model_name}-any-py3-none-any.whl"
         cache_dir = os.environ["PIP_CACHE"]
         subprocess.check_call(
             [sys.executable, "-m", "pip", "install", "--cache-dir", cache_dir, package]
