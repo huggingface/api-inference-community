51d55928fc7c997a7a8be32819f66cf8f452f09e
nateraw <nxr9266@g.rit.edu>
2022-05-11 22:39:11 -0400

    :fire: remove unused pipelines from speechbrain

--- docker_images/speechbrain/app/pipelines/__init__.py
+++ docker_images/speechbrain/app/pipelines/__init__.py
@@ -5,7 +5,3 @@ from app.pipelines.audio_to_audio import AudioToAudioPipeline
 from app.pipelines.automatic_speech_recognition import (
     AutomaticSpeechRecognitionPipeline,
 )
-from app.pipelines.image_classification import ImageClassificationPipeline
-from app.pipelines.question_answering import QuestionAnsweringPipeline
-from app.pipelines.text_to_speech import TextToSpeechPipeline
-from app.pipelines.token_classification import TokenClassificationPipeline
