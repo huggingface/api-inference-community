809fafcca61118f0fae5ab59e064ab3ad1c07339
osanseviero <osanseviero@gmail.com>
2022-06-07 22:40:35 +0200

    Cleanup

--- docker_images/speechbrain/app/pipelines/text_to_speech.py
+++ docker_images/speechbrain/app/pipelines/text_to_speech.py
@@ -1,22 +1,24 @@
 from typing import Tuple
 
 import numpy as np
-import torch
+from app.pipelines import Pipeline
+
+import numpy as np
 from app.common import ModelType, get_type, get_vocoder_model_id
 from app.pipelines import Pipeline
-from speechbrain.pretrained import HIFIGAN, Tacotron2
+from speechbrain.pretrained import Tacotron2, HIFIGAN
 
 
 class TextToSpeechPipeline(Pipeline):
     def __init__(self, model_id: str):
         model_type = get_type(model_id)
-        print("MODEL TYPE", model_type)
         if model_type is ModelType.TACOTRON2:
-            print("INITIALIZING FROM", model_id)
             self.model = Tacotron2.from_hparams(source=model_id)
         else:
-            raise ValueError(f"{model_type.value} is invalid for text-to-speech")
-
+            raise ValueError(
+                f"{model_type.value} is invalid for text-to-speech"
+            )
+        
         vocoder_type = get_type(model_id, "vocoder_interface")
         vocoder_model_id = get_vocoder_model_id(model_id)
         if vocoder_type is ModelType.HIFIGAN:
@@ -28,6 +30,7 @@ class TextToSpeechPipeline(Pipeline):
 
         self.sampling_rate = self.model.hparams.sample_rate
 
+
     def __call__(self, inputs: str) -> Tuple[np.array, int]:
         """
         Args:
@@ -37,5 +40,5 @@ class TextToSpeechPipeline(Pipeline):
             A :obj:`np.array` and a :obj:`int`: The raw waveform as a numpy array, and the sampling rate as an int.
         """
         mel_output, _, _ = self.model.encode_text("Example text!")
-        waveforms = self.vocoder_model.decode_batch(mel_output).numpy()
+        waveforms = self.vocoder_model.decode_batch(mel_output)
         return waveforms, self.sampling_rate
