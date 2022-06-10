fa508f14d60e8d7bf7b69e41189335f63e442224
merveenoyan <merveenoyan@gmail.com>
2022-06-10 14:39:46 +0200

    style

--- docker_images/sklearn/app/pipelines/__init__.py
+++ docker_images/sklearn/app/pipelines/__init__.py
@@ -1,5 +1,3 @@
 from app.pipelines.base import Pipeline, PipelineException  # isort:skip
 
-from app.pipelines.tabular_data_pipeline import (
-    TabularDataPipeline,
-)
+from app.pipelines.tabular_data_pipeline import TabularDataPipeline
