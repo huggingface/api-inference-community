ab10fb95406e8bb7faeaa03b818b177186f40c04
adrinjalali <adrin.jalali@gmail.com>
2022-07-04 18:33:07 +0200

    move the env build logic to the container

--- docker_images/sklearn/run_app.sh
+++ docker_images/sklearn/run_app.sh
@@ -1,10 +1,35 @@
-#!/bin/bash
-# This file is needed to first activate the environment created for this
-# project and then run the web server. The environment is created by the
-# `env_setup.py` script.
+#!/bin/bash --login
+# This file creates an environment with all required dependencies for the given
+# model, and then runs the start command.
 
-conda init bash
-source ~/.bashrc
-conda activate api-inference-model-env
+# This makes it easy to see in logs what exactly is being run.
+set -xe
+
+# We download only the config file and use `jq` to extract the requirements.
+curl https://huggingface.co/$MODEL_ID/raw/main/config.json --output /tmp/config.json 
+
+requirements="pandas uvicorn gunicorn"
+# this next command is needed to run the while loop in the same process and
+# therefore modify the same $requirements variable. Otherwise the loop would be
+# a separate process and the variable wouldn't be accessible from this parent
+# process.
+shopt -s lastpipe
+jq '.sklearn.environment' /tmp/config.json | jq '.[]' | while read r; do
+    requirements+=" $r"
+done
+
+# not sure why these are required. But if they're not here, the string passed
+# to micromamba is kinda not parsable by it.
+requirements=$(echo "$requirements" | sed "s/'//g")
+requirements=$(echo "$requirements" | sed "s/\"//g")
+
+micromamba create -c conda-forge -y -q --name=api-inference-model-env $requirements
+
+micromamba activate api-inference-model-env
+
 pip install api-inference-community
-python run_app.py
+
+# This file is not in our repos, rather taken from the
+# `uvicorn-gunicorn-docker` repos. You can check the Dockerfile to see where
+# exactly they are coming from.
+/start.sh
