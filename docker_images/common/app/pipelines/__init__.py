9e4f15e3fb5281b9e95f7f52abb0bedd777618b6
osanseviero <osanseviero@gmail.com>
2022-07-05 15:23:01 +0200

    Proof of concept for diffusers

--- docker_images/common/app/pipelines/__init__.py
+++ docker_images/common/app/pipelines/__init__.py
@@ -11,5 +11,6 @@ from app.pipelines.question_answering import QuestionAnsweringPipeline
 from app.pipelines.sentence_similarity import SentenceSimilarityPipeline
 from app.pipelines.speech_segmentation import SpeechSegmentationPipeline
 from app.pipelines.tabular_data_pipeline import TabularDataPipeline
+from app.pipelines.text_to_image import TextToImagePipeline
 from app.pipelines.text_to_speech import TextToSpeechPipeline
 from app.pipelines.token_classification import TokenClassificationPipeline
---
+++ docker_images/diffusers/app/pipelines/text_to_image.py
@@ -0,0 +1,36 @@
+from typing import TYPE_CHECKING
+
+import numpy
+import numpy as np
+import torch
+from app.pipelines import Pipeline
+from diffusers import DiffusionPipeline
+from PIL import Image
+
+
+if TYPE_CHECKING:
+    from PIL import Image
+
+
+class TextToImagePipeline(Pipeline):
+    def __init__(self, model_id: str):
+        self.pipeline = DiffusionPipeline.from_pretrained(model_id)
+        self.generator = torch.manual_seed(42)
+
+    def __call__(self, inputs: str) -> "Image.Image":
+        """
+        Args:
+            inputs (:obj:`str`):
+                a string containing some text
+        Return:
+            A :obj:`PIL.Image` with the raw image representation as PIL.
+        """
+        kwargs = {"eta": 0.3, "guidance_scale": 6.0, "num_inference_steps": 5}
+        image = self.pipeline(
+            [inputs], generator=self.generator, torch_device="cpu", **kwargs
+        )
+
+        image_processed = image.cpu().permute(0, 2, 3, 1)
+        image_processed = image_processed * 255.0
+        image_processed = image_processed.numpy().astype(np.uint8)
+        return Image.fromarray(image_processed[0])
