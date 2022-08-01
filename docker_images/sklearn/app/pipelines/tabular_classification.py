50636eee0811685ea7739a3756d3576f298bfd7a
adrinjalali <adrin.jalali@gmail.com>
2022-08-01 16:41:52 +0200

    ENH sklearn: use columns from config.json

--- docker_images/sklearn/app/pipelines/tabular_classification.py
+++ docker_images/sklearn/app/pipelines/tabular_classification.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Dict, List, Union
 
 import joblib
+import pandas as pd
 from app.pipelines import Pipeline
 from huggingface_hub import snapshot_download
 
@@ -20,30 +21,67 @@ class TabularClassificationPipeline(Pipeline):
                 # project. If the project is created using `skops`, it should have
                 # this file.
                 config = json.load(f)
-            model_file = config["sklearn"]["model"]["file"]
         except Exception as e:
             print(e)
-            # If for whatever reason we fail to detect requirements of the project,
-            # we install the latest scikit-learn.
+            # If for whatever reason we fail to detect requirements of the
+            # project, we install the latest scikit-learn.
+            #
             # TODO: we should log this, or warn or something.
-            model_file = DEFAULT_FILENAME
+            config = dict()
 
-        self.model = joblib.load(open(Path(cached_folder) / model_file, "rb"))
+        self.model_file = (
+            config.get("sklearn", {}).get("model", {}).get("file", DEFAULT_FILENAME)
+        )
+
+        with warnings.catch_warnings(record=True) as record:
+            self.model = joblib.load(open(Path(cached_folder) / self.model_file, "rb"))
+            if len(record) > 0:
+                # if there's a warning while loading the model, we save it so
+                # that it can be raised to the user when __call__ is called.
+                self._load_warnings = record
+
+        # use column names from the config file if available, to give the data
+        # to the model in the right order.
+        self.columns = config.get("sklearn", {}).get("columns", None)
 
     def __call__(
         self, inputs: Dict[str, Dict[str, List[Union[str, float]]]]
     ) -> List[Union[str, float]]:
-        """
+        """Return the output of the model for given inputs.
+
         Args:
             inputs (:obj:`dict`):
                 a dictionary containing a key 'data' mapping to a dict in which
                 the values represent each column.
         Return:
-            A :obj:`list` of floats or strings: The classification output for each row.
+            A :obj:`list` of floats or strings: The classification output for
+            each row.
         """
-        column_values = list(inputs["data"].values())
-        rows = list(zip(*column_values))
-        # TODO: we should do sth with the warnings.
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore")
-            return self.model.predict(rows).tolist()
+        if self.columns:
+            # TODO: we should probably warn if columns are not configured, we
+            # really do need them.
+            given_cols = set(inputs["data"].keys())
+            expected = set(self.columns)
+            extra = given_cols - expected
+            missing = expected - given_cols
+            if extra:
+                warnings.warn(
+                    f"The following columns were given but not expected: {extra}"
+                )
+
+            if missing:
+                warnings.warn(
+                    f"The following columns were expected but not given: {missing}"
+                )
+
+        # We convert the inputs to a pandas DataFrame, and use self.columns
+        # to order the columns in the order they're expected, ignore extra
+        # columns given if any, and put NaN for missing columns.
+        data = pd.DataFrame(inputs["data"], columns=self.columns)
+        res = self.model.predict(data).tolist()
+
+        if self._load_warnings:
+            for w in self._load_warnings:
+                warnings.warn(w)
+
+        return res
