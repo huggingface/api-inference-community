41e0b12db1a257068661045d1a6a641e56ce5422
Merve Noyan <merveenoyan@gmail.com>
2022-09-05 11:28:51 +0200

    Update docker_images/sklearn/tests/generators/generate.py
    Co-authored-by: Benjamin Bossan <BenjaminBossan@users.noreply.github.com>

--- docker_images/sklearn/tests/generators/generate.py
+++ docker_images/sklearn/tests/generators/generate.py
@@ -228,7 +228,7 @@ def main(version):
         if task != "text-classification":
             sample = X_test.head(10)
         else:
-            sample = X_test[0:10]
+            sample = X_test[:10]
 
         # save model input, which are later used for tests
         input_name = INPUT_NAMES[task].format(version=version)
