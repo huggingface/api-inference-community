6b5c0e484ef6f89ecab20128f3fa8cee3beb98c4
Nicolas Patry <patry.nicolas@protonmail.com>
2022-09-27 11:01:01 +0200

    First step towards CD. (Only flair for now) (#107)
    * First step towards CD.
    
    * CD remote action.
    
    * Triggering in the PR.
    
    * Triggering remote action.
    
    * Fix yaml
    
    * New secret.
    
    * Full path of docker building+ upload + trigger remote action.
    
    * More logs to check the actual output of the build
    
    * Curly braces.
    
    * Interpolation in single quote
    
    * Removing testing things.
    
    * Use main branch to actually run the workflow.
    
    * Removing unused parts..

---
+++ build_docker.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import argparse
+import os
+import subprocess
+import sys
+import uuid
+
+
+def run(command):
+    print(" ".join(command))
+    p = subprocess.run(command)
+    if p.returncode != 0:
+        sys.exit(p.returncode)
+
+
+def build(framework: str):
+    DEFAULT_HOSTNAME = os.getenv("DEFAULT_HOSTNAME")
+    hostname = DEFAULT_HOSTNAME
+    tag_id = str(uuid.uuid4())[:5]
+    tag = f"{framework}-{tag_id}"
+    container_tag = f"{hostname}/api-inference-prod-community:{tag}"
+
+    command = ["docker", "build", f"docker_images/{framework}", "-t", container_tag]
+    run(command)
+
+    command = ["aws", "ecr", "get-login-password"]
+    ecr_login = subprocess.Popen(command, stdout=subprocess.PIPE)
+    docker_login = subprocess.Popen(
+        ["docker", "login", "-u", "AWS", "--password-stdin", hostname],
+        stdin=ecr_login.stdout,
+        stdout=subprocess.PIPE,
+    )
+    docker_login.communicate()
+
+    command = ["docker", "push", container_tag]
+    run(command)
+    return tag
+
+
+def main():
+    frameworks = {
+        dirname for dirname in os.listdir("docker_images") if dirname != "common"
+    }
+    framework_choices = frameworks.copy()
+    framework_choices.add("all")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "framework",
+        type=str,
+        choices=framework_choices,
+        help="Which framework image to build.",
+    )
+    parser.add_argument(
+        "--out",
+        type=str,
+        help="Where to store the new tags",
+    )
+    args = parser.parse_args()
+
+    branch = (
+        subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+        .decode("utf-8")
+        .strip()
+    )
+    if branch != "main":
+        raise Exception(f"Go to branch `main` ({branch})")
+
+    print("Pulling")
+    subprocess.run(["git", "pull"])
+
+    if args.framework == "all":
+        outputs = []
+        for framework in frameworks:
+            tag = build(framework)
+            outputs.append((framework, tag))
+
+    else:
+        tag = build(args.framework)
+        outputs = [(args.framework, tag)]
+
+    for (framework, tag) in outputs:
+        name = f"{framework.upper()}_CPU_TAG"
+        print(name, tag)
+        if args.out:
+            with open(args.out, "w") as f:
+                f.write(f"{name}={tag}\n")
+
+
+if __name__ == "__main__":
+    main()
