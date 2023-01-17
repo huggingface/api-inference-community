94f284c9e75df67e6f5f441cb57524af91f75ce6
Sijun He <sijun.he@hotmail.com>
2023-01-17 20:28:44 +0800

    got expected results

--- docker_images/paddlenlp/app/pipelines/zero_shot_classification.py
+++ docker_images/paddlenlp/app/pipelines/zero_shot_classification.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from app.pipelines import Pipeline
 from paddlenlp.taskflow import Taskflow
@@ -13,28 +13,25 @@ class ZeroShotClassificationPipeline(Pipeline):
             pred_threshold=0.0,  # so that it returns all predictions
         )
 
-    def __call__(self, inputs: str, parameters: Dict[str, Any]) -> List[Dict[str, Any]]:
+    def __call__(self, inputs: str, candidate_labels: Optional[List[str]] = None) -> List[Dict[str, Any]]:
         """
         Args:
             inputs (:obj:`str`): a string to be classified
-            parameters (:obj:`str`): a dict containing the following keys:
-                - "candidate_labels": Required. a list of strings that are potential classes for inputs. 
-                - "multi_label": Optional. Boolean that is set to True if classes can overlap. (Default: false) 
-
+            candidate_labels (:obj:`List[str]`): a list of strings that are potential classes for inputs. 
         Return:
             A :obj:`list`:. a list of dicts containing the following:
                 - "sequence": The string sent as an input
                 - "labels": The list of strings for labels that you sent (in order)
                 - "scores": a list of floats that correspond the the probability of label, in the same order as labels.
         """
-        if parameters["multi_label"] is True:
-            raise NotImplementedError("Muti label setting not implemented yet")
-        self.taskflow.set_schema(parameters["candidate_labels"])
+        if candidate_labels is None:
+            raise ValueError("'candidate_labels' is a required field")
+        self.taskflow.set_schema(candidate_labels)
         taskflow_results = self.taskflow(inputs)
         pipeline_results = {}
         labels = []
         scores = []
-        for result in taskflow_results[0]:
+        for result in taskflow_results[0]["predictions"]:
             labels.append(result["label"])
             scores.append(result["score"])
         pipeline_results["labels"] = labels
