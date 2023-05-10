daeb2706255897039ea424a7924daacb36c16ac8
alibool <65023386+alibool@users.noreply.github.com>
2023-05-10 22:37:18 +0800

    Update test_api.py

--- docker_images/mmclassification/tests/test_api.py
+++ docker_images/mmclassification/tests/test_api.py
@@ -9,7 +9,7 @@ from app.main import ALLOWED_TASKS, get_pipeline
 # Tests do not check the actual values of the model output, so small dummy
 # models are recommended for faster tests.
 TESTABLE_MODELS: Dict[str, str] = {
-    "image-classification": "alibool/resnet50_8xb256-rsb-a1-600e_in1k"
+    "image-classification": ["alibool/resnet50_8xb256-rsb-a1-600e_in1k"]
 }
 
 
