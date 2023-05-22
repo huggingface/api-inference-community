214fdce7c235c7aa7c3188ac2df0db3ae9236d72
Daniel van Strien <davanstrien@gmail.com>
2023-05-22 14:12:28 +0100

    small fixes

--- docker_images/bertopic/app/pipelines/text_classification.py
+++ docker_images/bertopic/app/pipelines/text_classification.py
@@ -9,10 +9,6 @@ class TextClassificationPipeline(Pipeline):
         self,
         model_id: str,
     ):
-        # IMPLEMENT_THIS
-        # Preload all the elements you are going to need at inference.
-        # For instance your model, processors, tokenizer that might be needed.
-        # This function is only called once, so do all the heavy processing I/O here
         self.model = BERTopic.load(model_id)
 
     def __call__(self, inputs: str) -> List[Dict[str, float]]:
