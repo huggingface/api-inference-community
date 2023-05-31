7266b5be2e7a127630218898f7942e86d0aa39b2
oOraph <13552058+oOraph@users.noreply.github.com>
2023-05-31 14:35:40 +0200

    Periodically unload model / reset worker if no request was received for a while (#275)
    Allows to free gpu resources
    The cost to move it back to gpu if already loaded in mem is only ~3s

--- docker_images/diffusers/app/main.py
+++ docker_images/diffusers/app/main.py
@@ -1,19 +1,11 @@
-# isort: off
-import logging
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    logger = logging.getLogger(__name__)
-else:
-    logger = logging.getLogger(__name__)
-# isort: on
+import asyncio
 import functools
+import logging
 import os
 from typing import Dict, Type
 
 from api_inference_community.routes import pipeline_route, status_ok
+from app import idle
 from app.pipelines import ImageToImagePipeline, Pipeline, TextToImagePipeline
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
@@ -77,12 +69,10 @@ app = Starlette(routes=routes, middleware=middleware)
 
 @app.on_event("startup")
 async def startup_event():
-    logger = logging.getLogger("uvicorn.access")
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-    logger.handlers = [handler]
-
+    reset_logging()
     # Link between `api-inference-community` and framework code.
+    if idle.UNLOAD_IDLE:
+        asyncio.create_task(idle.live_check_loop(), name="live_check_loop")
     app.get_pipeline = get_pipeline
     try:
         get_pipeline()
@@ -91,7 +81,16 @@ async def startup_event():
         pass
 
 
+def reset_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        force=True,
+    )
+
+
 if __name__ == "__main__":
+    reset_logging()
     try:
         get_pipeline()
     except Exception:
