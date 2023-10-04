ddaa2121931134c6bd942802de36ba227bfb0823
oOraph <13552058+oOraph@users.noreply.github.com>
2023-10-04 09:17:11 +0200

    LoRA mutualization: keep base model loaded, load/unload LoRA adapters on the fly (#334)
    * LoRA mutualization: keep base model loaded, load/unload LoRA adapters on the fly
    * Fuse/Unfuse LoRA weights with/from the bigger models

--- docker_images/diffusers/app/pipelines/text_to_image.py
+++ docker_images/diffusers/app/pipelines/text_to_image.py
@@ -4,7 +4,7 @@ import os
 from typing import TYPE_CHECKING
 
 import torch
-from app import idle, timing
+from app import idle, lora, timing
 from app.pipelines import Pipeline
 from diffusers import (
     AutoencoderKL,
@@ -22,10 +22,12 @@ if TYPE_CHECKING:
     from PIL import Image
 
 
-class TextToImagePipeline(Pipeline):
+class TextToImagePipeline(Pipeline, lora.LoRAPipelineMixin):
     def __init__(self, model_id: str):
-        use_auth_token = os.getenv("HF_API_TOKEN")
-        model_data = model_info(model_id, token=use_auth_token)
+        self.current_lora_adapter = None
+        self.model_id = None
+        self.use_auth_token = os.getenv("HF_API_TOKEN")
+        model_data = model_info(model_id, token=self.use_auth_token)
 
         kwargs = (
             {"safety_checker": None}
@@ -35,32 +37,15 @@ class TextToImagePipeline(Pipeline):
         if torch.cuda.is_available():
             kwargs["torch_dtype"] = torch.float16
 
-        model_type = None
-        is_diffusers_lora = any(
-            file.rfilename
-            in ("pytorch_lora_weights.bin", "pytorch_lora_weights.safetensors")
-            for file in model_data.siblings
-        )
         has_model_index = any(
             file.rfilename == "model_index.json" for file in model_data.siblings
         )
 
-        file_to_load = next(
-            (
-                file.rfilename
-                for file in model_data.siblings
-                if file.rfilename.endswith(".safetensors")
-            ),
-            None,
-        )
-
-        if is_diffusers_lora or "lora" in model_data.cardData.get("tags", []):
+        if self._is_lora(model_data):
             model_type = "LoraModel"
-            if not file_to_load and not is_diffusers_lora:
-                raise ValueError("No *.safetensors file found for your LoRA")
         elif has_model_index:
             config_file = hf_hub_download(
-                model_id, "model_index.json", token=use_auth_token
+                model_id, "model_index.json", token=self.use_auth_token
             )
             with open(config_file, "r") as f:
                 config_dict = json.load(f)
@@ -70,29 +55,27 @@ class TextToImagePipeline(Pipeline):
 
         if model_type == "LoraModel":
             model_to_load = model_data.cardData["base_model"]
+            self.model_id = model_to_load
             if not model_to_load:
                 raise ValueError(
                     "No `base_model` found. Please include a `base_model` on your README.md tags"
                 )
-            if model_to_load == "stabilityai/stable-diffusion-xl-base-1.0":
-                vae = AutoencoderKL.from_pretrained(
-                    "madebyollin/sdxl-vae-fp16-fix",
-                    torch_dtype=torch.float16,  # load fp16 fix VAE
-                )
-                kwargs["vae"] = vae
-                kwargs["variant"] = "fp16"
 
-            self.ldm = DiffusionPipeline.from_pretrained(
-                model_to_load, use_auth_token=use_auth_token, **kwargs
-            )
-            weight_name = file_to_load if not is_diffusers_lora else None
+            weight_name = self._get_lora_weight_name(model_data)
+            self._load_sd_with_sdxl_fix(model_to_load, **kwargs)
             self.ldm.load_lora_weights(
-                model_id, weight_name=weight_name, use_auth_token=use_auth_token
+                model_id, weight_name=weight_name, use_auth_token=self.use_auth_token
             )
+            self.ldm.fuse_lora()
+            self.current_lora_adapter = model_id
         else:
-            self.ldm = AutoPipelineForText2Image.from_pretrained(
-                model_id, use_auth_token=use_auth_token, **kwargs
-            )
+            if model_id == "stabilityai/stable-diffusion-xl-base-1.0":
+                self._load_sd_with_sdxl_fix(model_id, **kwargs)
+            else:
+                self.ldm = AutoPipelineForText2Image.from_pretrained(
+                    model_id, use_auth_token=self.use_auth_token, **kwargs
+                )
+            self.model_id = model_id
 
         self.is_karras_compatible = (
             self.ldm.__class__.__init__.__annotations__.get("scheduler", None)
@@ -105,6 +88,19 @@ class TextToImagePipeline(Pipeline):
 
         if not idle.UNLOAD_IDLE:
             self._model_to_gpu()
+
+    def _load_sd_with_sdxl_fix(self, model_id, **kwargs):
+        if model_id == "stabilityai/stable-diffusion-xl-base-1.0":
+            vae = AutoencoderKL.from_pretrained(
+                "madebyollin/sdxl-vae-fp16-fix",
+                torch_dtype=torch.float16,  # load fp16 fix VAE
+            )
+            kwargs["vae"] = vae
+            kwargs["variant"] = "fp16"
+
+        self.ldm = DiffusionPipeline.from_pretrained(
+            model_id, use_auth_token=self.use_auth_token, **kwargs
+        )
 
     @timing.timing
     def _model_to_gpu(self):
@@ -119,6 +115,8 @@ class TextToImagePipeline(Pipeline):
         Return:
             A :obj:`PIL.Image.Image` with the raw image representation as PIL.
         """
+        self._load_lora_adapter(kwargs)
+
         if idle.UNLOAD_IDLE:
             with idle.request_witnesses():
                 self._model_to_gpu()
