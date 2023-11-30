1be0de29386c7e3c30f5a4c40a749c2e7b4540bf
apolinário <joaopaulo.passos@gmail.com>
2023-11-30 12:16:56 +0100

    add pivotal loading

--- docker_images/diffusers/app/pipelines/text_to_image.py
+++ docker_images/diffusers/app/pipelines/text_to_image.py
@@ -14,7 +14,7 @@ from diffusers import (
 )
 from diffusers.schedulers.scheduling_utils import KarrasDiffusionSchedulers
 from huggingface_hub import hf_hub_download, model_info
-
+from safetensors.torch import load_file
 
 logger = logging.getLogger(__name__)
 
@@ -60,12 +60,29 @@ class TextToImagePipeline(Pipeline, lora.LoRAPipelineMixin):
                 raise ValueError(
                     "No `base_model` found. Please include a `base_model` on your README.md tags"
                 )
-
+            
             weight_name = self._get_lora_weight_name(model_data)
             self._load_sd_with_sdxl_fix(model_to_load, **kwargs)
             self.ldm.load_lora_weights(
                 model_id, weight_name=weight_name, use_auth_token=self.use_auth_token
             )
+            if model_to_load == "stabilityai/stable-diffusion-xl-base-1.0":
+                if self._is_pivotal_tuning_lora(model_data):
+                    embedding_path = hf_hub_download(repo_id=model_id,
+                                 filename="embeddings.safetensors" if self._is_safetensors_pivotal(model_data) else "embeddings.pti",
+                                 repo_type="model")
+                    embeddings = load_file(embedding_path)
+                    state_dict_clip_l = embedding.get("text_encoders_0") if "text_encoders_0" in embedding else embedding.get("clip_l", None)
+                    state_dict_clip_g = embedding.get("text_encoders_0") if "text_encoders_0" in embedding else embedding.get("clip_g", None)
+                    if(state_dict_clip_l and len(state_dict_clip_l) > 0):
+                        tokens_to_add = len(state_dict_clip_l)
+                        token_list = [f"<s{i}>" for i in range(tokens_to_add)]
+                        self.ldm.load_textual_inversion(state_dict_clip_l, token=[token_list], text_encoder=self.ldm.text_encoder, tokenizer=self.ldm.tokenizer)
+                    if(state_dict_clip_g and len(state_dict_clip_g) > 0):
+                        tokens_to_add = len(state_dict_clip_g)
+                        token_list = [f"<s{i}>" for i in range(tokens_to_add)]
+                        self.ldm.load_textual_inversion(state_dict_clip_g, token=[token_list], text_encoder=self.ldm.text_encoder_2, tokenizer=self.ldm.tokenizer_2)
+            
             self.current_lora_adapter = model_id
             self._fuse_or_raise()
             logger.info("LoRA adapter %s loaded", model_id)
