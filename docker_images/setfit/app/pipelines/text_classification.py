0f460a2c8f5b0a3559e70fc061ef0413fb9fbdc3
Tom Aarsen <Cubiegamedev@gmail.com>
2023-12-04 09:25:17 +0100

    Prevent crash if id2label is None
    This may be the case on old models

--- docker_images/setfit/app/pipelines/text_classification.py
+++ docker_images/setfit/app/pipelines/text_classification.py
@@ -23,7 +23,7 @@ class TextClassificationPipeline(Pipeline):
         """
         probs = self.model.predict_proba([inputs], as_numpy=True)
         if probs.ndim == 2:
-            id2label = getattr(self.model, "id2label", {})
+            id2label = getattr(self.model, "id2label", {}) or {}
             return [
                 [
                     {"label": id2label.get(idx, idx), "score": prob}
