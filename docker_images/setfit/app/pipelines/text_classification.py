6b88d3ad807679c3564f105a5b8ea21b6b89b11d
Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
2023-12-04 08:10:49 +0100

    Apply Omar's suggestions
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>

--- docker_images/setfit/app/pipelines/text_classification.py
+++ docker_images/setfit/app/pipelines/text_classification.py
@@ -17,15 +17,13 @@ class TextClassificationPipeline(Pipeline):
             inputs (:obj:`str`):
                 a string containing some text
         Return:
-            A :obj:`list`:. The object returned should be a list of one list like [[{"label": 0.9939950108528137}]] containing:
+            A :obj:`list`: The object returned should be a list of one list like [[{"label": 0.9939950108528137}]] containing:
                 - "label": A string representing what the label/class is. There can be multiple labels.
                 - "score": A score between 0 and 1 describing how confident the model is for this label/class.
         """
         probs = self.model.predict_proba([inputs], as_numpy=True)
         if probs.ndim == 2:
-            id2label = {}
-            if hasattr(self.model, "id2label"):
-                id2label = self.model.id2label
+            getattr(self.model, "id2label", {})
             return [
                 [
                     {"label": id2label.get(idx, idx), "score": prob}
