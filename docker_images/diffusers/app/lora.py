5f4092bad362d5203044846cae7e0c0edfc674ba
apolinário <joaopaulo.passos@gmail.com>
2023-12-06 21:02:36 +0100

    Support pivotal tuning

--- docker_images/diffusers/app/lora.py
+++ docker_images/diffusers/app/lora.py
@@ -1,6 +1,8 @@
 import logging
 
-from huggingface_hub import model_info
+import torch.nn as nn
+from huggingface_hub import hf_hub_download, model_info
+from safetensors.torch import load_file
 
 
 logger = logging.getLogger(__name__)
@@ -12,6 +14,8 @@ class LoRAPipelineMixin(object):
             self.current_lora_adapter = None
         if not hasattr(self, "model_id"):
             self.model_id = None
+        if not hasattr(self, "current_tokens_loaded"):
+            self.current_tokens_loaded = 0
 
     @staticmethod
     def _get_lora_weight_name(model_data):
@@ -44,6 +48,20 @@ class LoRAPipelineMixin(object):
         )
         return is_diffusers_lora
 
+    @staticmethod
+    def _is_safetensors_pivotal(model_data):
+        embeddings_safetensors_exists = any(
+            sibling.rfilename == "embeddings.safetensors"
+            for sibling in model_data.siblings
+        )
+        return embeddings_safetensors_exists
+
+    @staticmethod
+    def _is_pivotal_tuning_lora(model_data):
+        return LoRAPipelineMixin._is_safetensors_pivotal(model_data) or any(
+            sibling.rfilename == "embeddings.pti" for sibling in model_data.siblings
+        )
+
     def _fuse_or_raise(self):
         try:
             self.ldm.fuse_lora(safe_fusing=True)
@@ -53,6 +71,74 @@ class LoRAPipelineMixin(object):
             self.ldm.unload_lora_weights()
             self.current_lora_adapter = None
             raise
+
+    def _reset_tokenizer_and_encoder(self, tokenizer, text_encoder, token_to_remove):
+        token_id = tokenizer(token_to_remove)["input_ids"][1]
+        del tokenizer._added_tokens_decoder[token_id]
+        del tokenizer._added_tokens_encoder[token_to_remove]
+        tokenizer._update_trie()
+
+        tokenizer_size = len(tokenizer)
+        text_embedding_dim = text_encoder.get_input_embeddings().embedding_dim
+        text_embedding_weights = text_encoder.get_input_embeddings().weight[
+            :tokenizer_size
+        ]
+        text_embeddings_filtered = nn.Embedding(tokenizer_size, text_embedding_dim)
+        text_embeddings_filtered.weight.data = text_embedding_weights
+        text_encoder.set_input_embeddings(text_embeddings_filtered)
+
+    def _unload_textual_embeddings(self):
+        if self.current_tokens_loaded > 0:
+            for i in range(self.current_tokens_loaded):
+                token_to_remove = f"<s{i}>"
+                self._reset_tokenizer_and_encoder(
+                    self.ldm.tokenizer, self.ldm.text_encoder, token_to_remove
+                )
+                self._reset_tokenizer_and_encoder(
+                    self.ldm.tokenizer_2, self.ldm.text_encoder_2, token_to_remove
+                )
+
+    def _load_textual_embeddings(self, adapter, model_data):
+        if self._is_pivotal_tuning_lora(model_data):
+            embedding_path = hf_hub_download(
+                repo_id=adapter,
+                filename="embeddings.safetensors"
+                if self._is_safetensors_pivotal(model_data)
+                else "embeddings.pti",
+                repo_type="model",
+            )
+            embeddings = load_file(embedding_path)
+            state_dict_clip_l = (
+                embeddings.get("text_encoders_0")
+                if "text_encoders_0" in embeddings
+                else embeddings.get("clip_l", None)
+            )
+            state_dict_clip_g = (
+                embeddings.get("text_encoders_0")
+                if "text_encoders_0" in embeddings
+                else embeddings.get("clip_g", None)
+            )
+
+            if state_dict_clip_l and len(state_dict_clip_l) > 0:
+                tokens_to_add = len(state_dict_clip_l)
+                token_list = [f"<s{i}>" for i in range(tokens_to_add)]
+                self.ldm.load_textual_inversion(
+                    state_dict_clip_l,
+                    token=[token_list],
+                    text_encoder=self.ldm.text_encoder,
+                    tokenizer=self.ldm.tokenizer,
+                )
+
+            if state_dict_clip_g and len(state_dict_clip_g) > 0:
+                tokens_to_add = len(state_dict_clip_g)
+                token_list = [f"<s{i}>" for i in range(tokens_to_add)]
+                self.ldm.load_textual_inversion(
+                    state_dict_clip_g,
+                    token=[token_list],
+                    text_encoder=self.ldm.text_encoder_2,
+                    tokenizer=self.ldm.tokenizer_2,
+                )
+            self.current_tokens_loaded = tokens_to_add
 
     def _load_lora_adapter(self, kwargs):
         adapter = kwargs.pop("lora_adapter", None)
@@ -76,6 +162,7 @@ class LoRAPipelineMixin(object):
                 )
                 self.ldm.unfuse_lora()
                 self.ldm.unload_lora_weights()
+                self._unload_textual_embeddings()
                 self.current_lora_adapter = None
                 logger.info("LoRA weights unloaded, loading new weights")
                 weight_name = self._get_lora_weight_name(model_data=model_data)
@@ -86,7 +173,10 @@ class LoRAPipelineMixin(object):
                 self.current_lora_adapter = adapter
                 self._fuse_or_raise()
                 logger.info("LoRA weights loaded for adapter %s", adapter)
-
+                self._load_textual_embeddings(adapter, model_data)
+                logger.info(
+                    "Textual Inversion embeddings loaded for adapter %s", adapter
+                )
             else:
                 logger.info("LoRA adapter %s already loaded", adapter)
         elif self.current_lora_adapter is not None:
@@ -97,3 +187,4 @@ class LoRAPipelineMixin(object):
             self.ldm.unfuse_lora()
             self.ldm.unload_lora_weights()
             self.current_lora_adapter = None
+            self.current_tokens_loaded = 0
